cb050f1fabae5208aca48a9c2fe1460d1e0dfb45
dimagisihilel <gunarathneedirimuni@gmail.com>
2025-02-27 14:28:02 +0530

    connect sign up with backend

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,3 +1,4 @@
+/*
 import {configureStore} from "@reduxjs/toolkit";
 
 import authReducer from "../reducers/AuthSlice.ts";
@@ -18,4 +19,33 @@ const store = configureStore({
         log:logReducer
     }
 });
+export default store;*/
+
+
+import {configureStore} from "@reduxjs/toolkit";
+
+import authReducer from "../reducers/AuthSlice.ts";
+import fieldReducer from "../reducers/FieldSlice.ts";
+import cropReducer from "../reducers/CropSlice.ts";
+import staffReducer from "../reducers/StaffSlice.ts";
+import vehicleReducer from "../reducers/VehicleSlice.ts";
+import equipmentReducer from "../reducers/EquipmentSlice.ts";
+import logReducer from "../reducers/LogSlice.ts";
+export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        field: fieldReducer,
+        crop: cropReducer,
+        staff: staffReducer,
+        vehicle: vehicleReducer,
+        equipment: equipmentReducer,
+        log: logReducer
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false, // Disable serializability check for FormData
+        }),
+});
+
+export type AppDispatch = typeof store.dispatch;
 export default store;
